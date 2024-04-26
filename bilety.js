7dd0392cb4afe7143b0cdeafd3428d7591bb0a70
ghdyo1 <134512477+ghdyo1@users.noreply.github.com>
2024-04-27 01:08:15 +0300

    превью билета

--- bilety.js
+++ bilety.js
@@ -1,8 +1,16 @@
 let btn = document.querySelector("#buy");
+let view = document.querySelector("#view");
+let ticket = document.querySelector(".card img")
 let warn = document.querySelector(".warn-wrapper");
 
 function buy(){
     warn.style.animationName = "card-reveal";
 }
+function ticketView(){
+    ticket.style.animationName = "card-reveal";
+    view.style.display = "none";
+    btn.style.display = "block";
+}
 
 btn.addEventListener("click", buy);
+view.addEventListener("click", ticketView);
